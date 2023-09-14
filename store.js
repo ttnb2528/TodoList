a5c818cb1a86c7db3b3f546d6b6e1cfdee9877ac
ttnb2528 <thientan2812@gmail.com>
2023-09-14 19:01:22 +0700

    first commit

---
+++ store.js
@@ -0,0 +1,9 @@
+import { createStore } from "./core.js";
+import reducer from "./reducer.js";
+import widthLogger from "./widthLogger.js"
+
+const { attach, connect, dispatch } = createStore(widthLogger(reducer));
+
+window.dispatch = dispatch;
+
+export { attach, connect }; 
